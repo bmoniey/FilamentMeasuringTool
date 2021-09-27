8ed13fc6e3f91e4cf8a4a56be2508f5da2af5f23
b.moniey <b.moniey@gmail.com>
2021-09-26 19:33:52 -0700

    added comments for the purpose

--- fmt20/Adafruit_SSD1306_D.h
+++ fmt20/Adafruit_SSD1306_D.h
@@ -1,3 +1,14 @@
+/**
+ * Purpose:
+ * Adafruit_SSD1306_D inherits from the standard Adafruit_SSD1306D
+ * and add methods: 
+ *  display_d()
+ *    This version allows a void func(void) pointer to be passed. The purpose of this is to allow the encoder position
+ *    to be updated after every line update on the display. This was to prevent long updates of the screen (35ms) from causing misse
+ *    wrapping of the encoder position.
+ * and overloads:
+ *  begin()
+ */
 #ifndef _ADAFRUIT_SSD1306_D_H_
 #define _ADAFRUIT_SSD1306_D_H_
   #include <Arduino.h>
